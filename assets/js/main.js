69e53f0458e4c54c3460d296bee8ee0796e5f818
Mostafa Nabil <60539423+MostafaNabilll@users.noreply.github.com>
2024-03-28 00:36:43 +0100

    Add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,6 +107,6 @@ const sr = ScrollReveal({
 
 sr.reveal('.home__data, .home__social, .contact__container, .footer__container');
 sr.reveal('.home__image', {origin:'bottom'});
-sr.reveal('.about__data, .skills__data', {origin:'left'});
-sr.reveal('.about__image, .certifications', {origin:'right'}); // Update target
+sr.reveal('.about__data, .skills__data, .skills__content', {origin:'left'});
+sr.reveal('.about__image, .certifications__list', {origin:'right'});
 sr.reveal('.services__card, .projects__card' , {interval:100});
